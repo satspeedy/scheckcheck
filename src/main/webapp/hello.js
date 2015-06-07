2ee6843db6a864315e36ef1b5d6a36b740575502
satspeedy <satspeedy@hotmail.com>
2015-06-07 11:32:43 +0200

    adjusts urls

--- src/main/webapp/hello.js
+++ src/main/webapp/hello.js
@@ -2,7 +2,7 @@
  * Created by HHA on 07.04.2015.
  */
 function Hello($scope, $http) {
-    $http.get('http://scheckcheck-satspeedy.rhcloud.com/scheckcheck/resources/teilnehmer/1').
+    $http.get('http://scheckcheck-satspeedy.rhcloud.com/resources/teilnehmer/1').
         success(function(data) {
             $scope.teilnehmer = data;
         });
--- src/main/webapp/list.js
+++ src/main/webapp/list.js
@@ -2,7 +2,7 @@
  * Created by HHA on 07.04.2015.
  */
 function List($scope, $http) {
-    $http.get('http://scheckcheck-satspeedy.rhcloud.com/scheckcheck/resources/teilnehmer').
+    $http.get('http://scheckcheck-satspeedy.rhcloud.com/resources/teilnehmer').
         success(function(data) {
             $scope.teilnehmers = data;
         });
