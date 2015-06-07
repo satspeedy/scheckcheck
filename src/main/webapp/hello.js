aa599ae97b8c9e81361467011547914e3ad547f6
satspeedy <satspeedy@hotmail.com>
2015-06-07 10:26:42 +0200

    adds cors filter and change url to server in html files

--- src/main/webapp/hello.js
+++ src/main/webapp/hello.js
@@ -2,7 +2,7 @@
  * Created by HHA on 07.04.2015.
  */
 function Hello($scope, $http) {
-    $http.get('http://localhost:8080/scheckcheck/resources/teilnehmer/1').
+    $http.get('http://scheckcheck-satspeedy.rhcloud.com/scheckcheck/resources/teilnehmer/1').
         success(function(data) {
             $scope.teilnehmer = data;
         });
